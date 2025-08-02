a0164ff9074fee30c827cc12305efc99984c8e64
imaginingforward <thusoozooni@gmail.com>
2025-08-02 11:05:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -276,17 +276,17 @@ def parse():
     # 4. Add location filters
     if "hq_state" in filters:
         must_clauses.append({
-            "term": {"hq_state.keyword": filters["hq_state"].lower()}
+            "match": {"hq_state": filters["hq_state"]}
         })
         
     if "hq_country" in filters:
         must_clauses.append({
-            "term": {"hq_country.keyword": filters["hq_country"].lower()}
+            "match": {"hq_country": filters["hq_country"]}
         })
         
     if "hq_city" in filters:
         must_clauses.append({
-            "term": {"hq_city.keyword": filters["hq_city"].lower()}
+            "match": {"hq_city": filters["hq_city"]}
         })
 
     # 5. Add funding stage filter (i.e. series A companies)
